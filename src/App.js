e27df5485fd87f9654b18de238e1f2bd6ad4a045
Abram Felix <abramfelix1@gmail.com>
2023-10-01 12:12:59 -0700

    update

--- src/App.js
+++ src/App.js
@@ -1,14 +1,7 @@
-import { useState } from "react";
-import { Canvas } from "@react-three/fiber";
-import Landing from "./components/Landing.jsx";
 import { Routes, Route } from "react-router-dom";
 import Main from "./components/Main.jsx";
 
-const root = document.getElementById("root");
-
 function App() {
-  const [onLoad, setOnLoad] = useState(true);
-
   return (
     <Routes>
       <Route path="/" element={<Main />} />
